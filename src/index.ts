8e626c55e077869022c81471435d5ac329e6fedd
ia <isaac.ardis@gmail.com>
2019-10-25 17:38:53 -0500

    minor tidies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-// import ServiceRunner from "../build/generated-client/typescript";
-// import EthClient from "../build/example-client/build";
-
 import ServiceRunner from "@etclabscore/jade-service-runner-client";
 import { EthereumJSONRPC } from "@etclabscore/ethereum-json-rpc"; // <-- which one is it?
-
 
 const client = new ServiceRunner({
   transport: {
@@ -29,31 +25,32 @@ const erpcGoerli = new EthereumJSONRPC({
     type: "http",
     path: "multi-geth/goerli/1.9.2",
   },
-  
-})
+});
 
 const clients = {
- "goerli" : erpcGoerli,
+  "goerli" : erpcGoerli,
   "kotti" : erpcKotti,
-}
+};
 
-
-const setupClients = async (environments: string[]) {
+const setupClients = async (environments: string[]) => {
   const result = await client.installService("multi-geth", "1.9.2");
   console.log("result"); //tslint:disable-line
   environments.forEach(async (env)=>{
       await client.startService("multi-geth", "1.9.2", env); 
   });
-  
-}
+};
 
-const txHashKotti = "something";
+// const txHashKotti = "something";
 
-async exec () {
+const exec = async () => {
  await setupClients(Object.keys(clients));
- const balance = await client.kotti.eth_getBalance("0xc1912fee45d61c87cc5ea59dae31190fffff232d", "0x0");
+//  const balance = await clients.kotti.eth_getBalance("0xc1912fee45d61c87cc5ea59dae31190fffff232d", "0x0");
  // use BigNumber.js I think to pull this back ~ npm install  @types/bignumber.js 
- const txReciept = await clients.goerli.eth_sendRawTransaction(txHashKotti);
-}
+  // console.log(balance);
+ //  const txReciept = await clients.goerli.eth_sendRawTransaction(txHashKotti);
+
+  let blockN = await clients.kotti.eth_getBlockByNumber("latest", false);
+  console.log("blockn", blockN);
+};
 
 exec();
